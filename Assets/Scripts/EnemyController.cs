81a1b7b9d5c15668abc9d5780eb5f7ed6049e070
CrazyPumpkinPie <103962183+CrazyPumpkinPie@users.noreply.github.com>
2023-03-29 21:16:58 +0300

    v2

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,36 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EnemyController : MonoBehaviour
 {
-    private float speed = 18;
+    private float speed = 3;
     private float leftBound = -1;
 
-    public static float hp = 100, attack = 20;
+    public static float hp = 80, attack = 3;
+
+    [SerializeField] GameObject flyingText;
+
+    private Animator enemyAnim;
+
     void Start()
     {
-        
+        enemyAnim = GetComponent<Animator>();
+
+
     }
 
     void Update()
     {
         MoveForward();
-
-        if (transform.position.x <= leftBound)
-            GameManager.Instance.isFighting = true;
     }
 
     void MoveForward()
     {
-        if (!GameManager.Instance.isGameOver && !GameManager.Instance.isFighting && transform.position.x >= leftBound)
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
-    }
+        if (!GameManager.Instance.isGameOver)
+        {
+            if (transform.position.x >= leftBound)
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                enemyAnim.SetFloat("Speed_f", 1.0f);
+            }
+            else
+            {
+                GameManager.Instance.isFighting = true;
+                enemyAnim.SetFloat("Speed_f", 0f);
+                enemyAnim.SetInteger("WeaponType_int", 10);
+            }
+        }
+        else
+        {
+            enemyAnim.SetFloat("Speed_f", 0f);
+            enemyAnim.SetInteger("WeaponType_int", 0);
+        }
+
+     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Projectile"))
+        {
+            Healthbar.UpdateHealth(gameObject, PlayerController.attack);
+            other.gameObject.SetActive(false);
 
-        Destroy(other.gameObject);
+            GameObject damageNumber = Instantiate(flyingText, transform.position + new Vector3(0, 3, 0), flyingText.transform.rotation);
+            damageNumber.transform.GetChild(0).GetComponent<TextMeshPro>().SetText(PlayerController.attack.ToString());
+            //FlyingNumbers.Instance.SendFlyingText(PlayerController.attack,transform.position + new Vector3(0,2,0));
+        }
     }
+
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public static float hp=100,attack = 50;
+    public static float hp = 100,hpReg = 3, attack = 50,attackSPD = 0.6f;
+    private float attackDistance = 15;
 
+    [SerializeField] private GameObject projectile;
+    private GameObject[] projectilePool;
+    private int currentProjectile = 0, projectilePoolSize = 15;
+
+    private Rigidbody projectileRb;
+    private bool isProjectileFlying,isGettingDamage,isHealing;
+
+    private static PlayerController player;
+    public static PlayerController Player { get { return player; }}
+    private void Awake()
+    {
+        player = this;
+    }
     void Start()
     {
-        
+        isProjectileFlying = false;
+        isGettingDamage = false;
+        isHealing = false;
+
+        projectileRb = projectile.GetComponent<Rigidbody>();
+        projectilePool = new GameObject[projectilePoolSize];
+
+        for (int i = 0; i < projectilePoolSize; i++)
+        {
+            projectilePool[i] = Instantiate(projectile);
+            projectilePool[i].gameObject.SetActive(false);
+        }
+
+        Healthbar.SetNewHP(gameObject);
     }
 
     void Update()
     {
+        if (!isProjectileFlying && !GameManager.Instance.isGameOver && GameObject.FindGameObjectWithTag("Enemy").transform.position.x - projectileRb.position.x <= attackDistance)
+            StartCoroutine(Shoot());
+        else StopCoroutine(Shoot());
+
+        if (GameManager.Instance.isFighting && !isGettingDamage && !GameManager.Instance.isGameOver)
+            StartCoroutine(Fight());
+        else StopCoroutine(Fight());
+
+        if (gameObject.GetComponent<Healthbar>().healthbar.value < gameObject.GetComponent<Healthbar>().healthbar.maxValue && !isHealing && !GameManager.Instance.isGameOver)
+            StartCoroutine(HPRegen());
+        else StopCoroutine(HPRegen());
+    }
+    IEnumerator Shoot()
+    {
+        if (currentProjectile >= projectilePoolSize)
+            currentProjectile = 0;
+
+        isProjectileFlying = true;
         
+        projectilePool[currentProjectile].SetActive(true);
+        projectilePool[currentProjectile].transform.position = transform.position + new Vector3(0,1,0);
+        currentProjectile++;
+        yield return new WaitForSeconds(1/attackSPD);
+
+        isProjectileFlying = false;
+    }
+    IEnumerator Fight()
+    {
+        isGettingDamage = true;
+
+        Healthbar.UpdateHealth(gameObject, EnemyController.attack * spawnManager.enemyCount);
+        yield return new WaitForSeconds(1f);
+
+        isGettingDamage = false;
+    }
+    IEnumerator HPRegen()
+    {
+        isHealing = true;
+
+        gameObject.GetComponent<Healthbar>().healthbar.value += hpReg;
+        yield return new WaitForSeconds(1);
+
+        isHealing = false;
     }
 }
